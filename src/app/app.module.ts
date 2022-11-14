ee0aa8d216310f6264a467f1644414d9b11b921a
Burak Özel <burakk.oozel@gmail.com>
2022-11-14 18:57:38 +0300

    created home,login and register pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './pages/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(
+      {positionClass: 'toast-top-right'},
+    ),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup; //kullanıcı girişi için oluşturulacak formgroup
+
+  constructor(
+    private formBuilder:FormBuilder,//angular form oluşturmak için ilgili servis
+    private toastr:ToastrService,//kullanıcı mesajlarını göstermek için yüklemiş olduğumuz npm paket..
+    private usersService:UsersService// user httpreq. işlemleri için oluşturulan servis..
+    ) { }
+
+  ngOnInit(): void {
+    this.createLoginForm();//loginformu oluşturulacak metot sayfa ilk yüklendiğinde onInit içinde çağrılır...
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({//loginform inputları için başlangıç değeri ve validasyon ataması burada yapılır ayrıca burdaki key değerleri html tarafında formcontrolname için gereklidir......
+      email: ['', [Validators.required,Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  login(){
+    if(!this.loginForm.valid){//form invalid ise toastr ile kullanıcıya hata göster
+      this.toastr.error('Form alanı zorunludur', 'Sistem mesajı :');
+    }else{
+      this.usersService.getUser(this.loginForm.value.email).subscribe({//userservice'den queryparams ile girilen maili içeren datayı get eder...
+        next: (res) => {
+          if(res.length === 0){//data yoksa length sıfır gelir.. hata göster..
+            this.toastr.error("Böyle bir hesap bulunamadı...","Sistem mesajı");
+          }else{//ilgili data varsa
+            if(res[0].password == this.loginForm.value.password){//girilen şifre get edilen data'nın şifresi ile aynı mı?
+              this.toastr.success("Başarılı bir şekilde giriş yapıldı...")
+            }else{
+              this.toastr.error("Email yada şifre hatalı...","Sistem mesajı");
+            }
+          }
+        },
+        error: (err) => {
+          console.log(err);//hata varsa consola bas
+        },
+        complete: () => {
+          //Todo:homepage yönlendir...
+        }
+      })
+    }
+  }
+
+}
