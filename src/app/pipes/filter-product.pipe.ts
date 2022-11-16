a16c3d43e47d40027561bc05d6244af135683608
Burak Özel <burakk.oozel@gmail.com>
2022-11-16 13:13:03 +0300

    created custom pipe for input search

---
+++ src/app/pipes/filter-product.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../models/product';
+
+@Pipe({
+  name: 'filterProduct'
+})
+export class FilterProductPipe implements PipeTransform {
+
+  transform(value: Product[], searchedWord:string) {
+    if(!searchedWord) return value;
+    
+    return value.filter((product) => product.name.toLocaleLowerCase().includes(searchedWord.toLowerCase()));
+  }
+
+}
